897d7a01d7e6d6b8d2fbb89f487480ca02d3dd40
rundll <nomad@ag.ru>
2022-07-17 19:57:39 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void run() {
+    }
+
+}
