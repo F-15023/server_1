e30852808a8d8834c29d8774cf7f83bd3ea72e37
rundll <nomad@ag.ru>
2022-07-17 23:01:27 +0300

    netty принимает сообщения https://www.youtube.com/watch?v=2UyJRQMrosA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,49 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class Main {
 
     public static void main(String[] args) {
         try {
-            run();
+            startNetty();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     private static void run() {
+    }
+
+    private static void startNetty() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new MainInboundHandler());
+                        }
+                    });
+            ChannelFuture future = serverBootstrap.bind(12345).sync();
+            future.channel().closeFuture().sync();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
     }
 
 }
